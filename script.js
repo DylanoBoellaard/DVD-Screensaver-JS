ad8e3de97dfa2a7b93f213909ee1ca539068bd76
DylanoBoellaard <336751@student.mboutrecht.nl>
2024-12-10 23:19:14 +0100

    Added ability for logo to bounce off the browser window's walls

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Defines variables for the section and SVG DVD logo
 const section = document.querySelector('section');
 const logo = document.querySelector('#DVDLogo');
+const FPS = 60;
 
 // Get the current window height & width on the browser and adds 'px' for use in the stylesheet
 section.style.height = window.innerHeight + 'px';
@@ -9,8 +10,8 @@ section.style.width = window.innerWidth + 'px';
 // Variables to define position & speed of the DVD logo
 let xPosition = 10;
 let yPosition = 10;
-let xSpeed = 10;
-let ySpeed = 10;
+let xSpeed = 4;
+let ySpeed = 4;
 
 // Update function that will run ever 1 second.
 function update() {
@@ -20,8 +21,16 @@ function update() {
 
 // Function to run code every 1 second.
 setInterval(() => {
+    if (xPosition + logo.clientWidth >= window.innerWidth || xPosition <= 0) {
+        xSpeed = -xSpeed;
+    }
+
+    if (yPosition + logo.clientHeight >= window.innerHeight || yPosition <= 0) {
+        ySpeed = -ySpeed;
+    }
+
     xPosition += xSpeed;
     yPosition += ySpeed;
 
     update();
-}, 1000);
+}, 1000/FPS);
